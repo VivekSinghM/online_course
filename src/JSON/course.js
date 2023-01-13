68620d64c2a408668cb9b600b89dac050f96d830
vivek <viveksinghmongare@gmail.com>
2023-01-13 11:49:35 +0530

    added imgs

--- src/JSON/course.js
+++ src/JSON/course.js
@@ -11,7 +11,7 @@ export const data = {
         },
         {
             "name":"Sales vs marketing",
-            "img":"https://cdn.pixabay.com/photo/2018/08/14/13/23/ocean-3605547__480.jpg",
+            "img":"https://garden.spoonflower.com/c/13582821/p/f/m/wwxsE862a8auaZ3QhOJ_KxAWZnwfsY38GpGOLw77UGTOqFV2jU1xjz4/FLOWER%20PURPLE%20NAVY.jpg",
             "lastUpdated":"4/08/22",
             "lessions":3,
             "minutes":25,
@@ -20,7 +20,7 @@ export const data = {
         },
         {
             "name":"How to Unclog a Sink",
-            "img":"https://cdn.pixabay.com/photo/2018/08/14/13/23/ocean-3605547__480.jpg",
+            "img":"https://www.shutterstock.com/image-vector/silhouette-samurai-night-background-japanese-260nw-2195759651.jpg",
             "lastUpdated":"4/08/22",
             "lessions":6,
             "minutes":15,
@@ -29,7 +29,43 @@ export const data = {
         },
         {
             "name":"How Google Analytics Ruined the Marketing",
+            "img":"https://as1.ftcdn.net/v2/jpg/02/36/97/18/1000_F_236971875_9KGHijoVS8JPAicJmMCKqUWN099OVUgU.jpg",
+            "lastUpdated":"4/08/22",
+            "lessions":3,
+            "minutes":25,
+            "status":"LIVE",
+            "tags":["Sales","Marketing"]
+        },
+        {
+            "name":"XYZ",
             "img":"https://cdn.pixabay.com/photo/2018/08/14/13/23/ocean-3605547__480.jpg",
+            "lastUpdated":"4/08/22",
+            "lessions":1,
+            "minutes":13,
+            "status":"DRAFT",
+            "tags":["Marketing"]
+        },
+        {
+            "name":"Sales vs marketing",
+            "img":"https://garden.spoonflower.com/c/13582821/p/f/m/wwxsE862a8auaZ3QhOJ_KxAWZnwfsY38GpGOLw77UGTOqFV2jU1xjz4/FLOWER%20PURPLE%20NAVY.jpg",
+            "lastUpdated":"4/08/22",
+            "lessions":3,
+            "minutes":25,
+            "status":"ARCHIVED",
+            "tags":["Sales","Marketing"]
+        },
+        {
+            "name":"How to Unclog a Sink",
+            "img":"https://www.shutterstock.com/image-vector/silhouette-samurai-night-background-japanese-260nw-2195759651.jpg",
+            "lastUpdated":"4/08/22",
+            "lessions":6,
+            "minutes":15,
+            "status":"LIVE",
+            "tags":["Sales"]
+        },
+        {
+            "name":"How Google Analytics Ruined the Marketing",
+            "img":"https://as1.ftcdn.net/v2/jpg/02/36/97/18/1000_F_236971875_9KGHijoVS8JPAicJmMCKqUWN099OVUgU.jpg",
             "lastUpdated":"4/08/22",
             "lessions":3,
             "minutes":25,
